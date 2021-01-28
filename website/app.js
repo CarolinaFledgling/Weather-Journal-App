1a17758b33d44815e38146811a0f12d7be38a89f
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-01-28 19:43:45 +0100

    declared Global Variables in app.js

---
+++ website/app.js
@@ -0,0 +1,11 @@
+// Global Variables
+
+const inputZipCode = document.querySelector('.holder__input');
+const btn = document.querySelector('.btn');
+const descriptionTextarea = document.querySelector('.holder__textarea');
+const temperature = document.getElementById('temp');
+const date = document.getElementById('date');
+const city = document.getElementById('content');
+const weather = document.getElementById('weather');
+const describeAnswer = document.getElementById('describe');
+const photo = document.querySelector('.entry__photo')
