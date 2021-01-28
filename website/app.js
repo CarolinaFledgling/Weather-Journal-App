5f3d0a12d5ab3956be75c69948924dd952107628
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-01-28 19:53:43 +0100

    app.js : created a new data instance dynamically with JS , added url APi from openWeather.org

--- website/app.js
+++ website/app.js
@@ -9,3 +9,14 @@ const city = document.getElementById('content');
 const weather = document.getElementById('weather');
 const describeAnswer = document.getElementById('describe');
 const photo = document.querySelector('.entry__photo')
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+
+// API openWeather.org
+
+const apiLink = 'api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '5bb278a6a7c5f285bd3b3bfd9852892a';
+const units = '&units=metric';
